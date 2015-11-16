ee65d5b6a4fb22a0cad67ac6c038add051abc088
Oscar Nord <oscar.nord@gmail.com>
2015-11-16 20:13:07 +0100

    Update task 2 Lab 1

--- SimpleReplace.m
+++ SimpleReplace.m
@@ -62,54 +62,43 @@ Oimage = imread('/Users/Oscar/Documents/TNM087/Lab 1/Images/bild.jpg');
 
 %% Copy image and edit
 %
-% Specify rows and cols for test example
-rows = [50:80]; 
-cols = [50:80];
-
-%figure(1)
+figure(1)
 Nimage = Oimage;
 %imshow(Oimage);
 
 % change rows
-%Set specified rows to 255 (max)
-%Nimage(rows:rows:end,:,1) = 255; 
-Nimage(rows,:,1) = 255; 
+% Set Nimage to Oimage's red channel
+%Nimage = Nimage(1:end,:,1);
+
+%Test from stack
+Nimage(1:1:end,:,1) = 255; 
+    figure(2)
+    imshow(Nimage);
+
+% Set Rows red pixel value to 255 (max)
+Nimage(1:end,:) = 255;
+    figure(3)
+    imshow(Nimage);
+
+% Insert the red chanel in a copy of Oimage
+Fimage(:,:,3) = Oimage(:,:,3);
+Fimage(:,:,2) = Oimage(:,:,2);
+Fimage(:,:,1) = Nimage;
+
+    figure(4)
+    imshow(Fimage)
 
 % change cols
-%Nimage(:,cols:cols:end,2) = 255;
-Nimage(:,cols,2) = 255;  
+Nimage = Oimage(:,1:end,2);
+Nimage(:,1:end) = 255;
 
-inds = find(ismember(rows, cols));
-Wimage1 = intersect(rows,cols);
-%[C, ia, ib] = intersect(rows,cols);
-Nimage(Wimage1,Wimage1,:) = 255;
+Fimage(:,:,2) = Nimage;
 
-
-
+    %figure(5)
+    %imshow(Fimage)
 
 % change box
-%Nimage = Oimage;
-
-%   box: vector with four elements [sr,sc,size1,size2]
-%                       where (sr,sc) are coordinates of box origin
-%                       (size1, size2) is the size of the box 
-%                       origin and size use row/column convention
-
-%box = [sr,sc,size1,size2]
-
-% sr = 100;
-% sc = 100;
-% size1 = 80:100;
-% size2 = 80:120;
-% 
-% box = [sr, sc, size1, size2];
-% 
-% test = box(:,1);
-% 
-% Nimage(test,test:end,:) = 255;
-
-figure(3)
-imshow(Nimage);  
+%Nimage =
 
 % change grid points
 
@@ -121,73 +110,3 @@ imshow(Nimage);
 
 end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-%
-
-%rows = 20;
-%cols = 20;
-
-%figure(1)
-%Nimage = Oimage;
-%imshow(Oimage);
-
-% change rows
-% Set Nimage to Oimage's red channel
-%Nimage = Nimage(1:end,:,1);
-
-%Test from stack
-%Nimage(rows:rows:end,:,1) = 255; 
-    %figure(2)
-    %imshow(Nimage);
-
-% Set Rows red pixel value to 255 (max)
-%Nimage(1:end,:) = 255;
-    %figure(3)
-    %imshow(Nimage);
-
-% Insert the red chanel in a copy of Oimage
-
-%Fimage(:,:,3) = Oimage(:,:,3);
-%Fimage(:,:,2) = Oimage(:,:,2);
-%Fimage(:,:,1) = Nimage;
-
-    %figure(4)
-    %imshow(Fimage)
-
-% change cols
-
-%Nimage = Oimage(:,1:end,2);
-%Nimage(:,1:end) = 255;
-
-%Fimage(:,:,2) = Nimage;
-
-    %figure(5)
-    %imshow(Fimage)
-
-%Nimage(:,cols:cols:end,2) = 255;
-    
-%figure(3)
-%imshow(Nimage);    
-    
-    
-% change box
-%Nimage =
-
-% change grid points
-
-
-
-%indices = sub2ind(size(Nimage), rows,cols);
-%Nimage(indices) = 255;
